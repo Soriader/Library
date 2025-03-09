7e9aa472f7116f2c43ae3b7404931bd4c236e166
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-09 19:31:14 +0100

    [~] Created class 'LibraryDatabase'

---
+++ Library/LibraryDatabase.cs
@@ -0,0 +1,17 @@
+﻿namespace Library;
+
+using Microsoft.EntityFrameworkCore;
+using Library.Models;
+
+public class LibraryDatabase
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=LibraryApp;Integrated Security=True;");
+        }
+    }
+}
