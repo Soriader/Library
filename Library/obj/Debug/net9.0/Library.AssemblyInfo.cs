1d234df1a97e1360b5bee165e53dec41d66de85e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-09 19:29:45 +0100

    [~] Added correct NuGet and create folder 'Models' with class 'Book'

--- Library/obj/Debug/net9.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net9.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28ab15db5bae516988d8e4604ee0ef855f8ccdca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
